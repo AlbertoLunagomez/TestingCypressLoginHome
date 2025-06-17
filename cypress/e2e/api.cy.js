def22e7d18edbe7e9cd266e3109ab597dc2af20b
AlbertoLunagomez <alberto.lunagomez@viaro.net>
2025-06-17 17:00:16 +0000

    feat: add API code and header vlaidation

---
+++ cypress/e2e/api.cy.js
@@ -0,0 +1,13 @@
+describe('REST API TEST WITH CYPRESS', function() {
+    it('API Test-Header Validation', function(){
+        cy.request('https://pokeapi.co/api/v2/pokemon/33/').as('pokemon')
+        cy.get('@pokemon').its('headers').its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+    })
+
+    it('API Test-Status Validation', function(){
+        cy.request('https://pokeapi.co/api/v2/pokemon/33/').as('pokemon')
+        cy.get('@pokemon').its('status')
+            .should('equal', 200)
+    })
+})
