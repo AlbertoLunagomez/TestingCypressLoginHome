efa39ca38daaf72a58065e4e5025846040f44e72
AlbertoLunagomez <alberto.lunagomez@viaro.net>
2025-06-17 21:47:38 +0000

    ref: apply POM struture

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,18 +1,24 @@
 class LoginPage {
+  constructor() {
+    this.usernameInput = '#username';
+    this.passwordInput = '#password';
+    this.buttonSubmit = 'button[type="submit"]';
+  }
+
   visit() {
     cy.visit('/login');
   }
 
   fillUsername(username) {
-    cy.get('#username').type(username);
+    cy.get(this.usernameInput).type(username);
   }
 
   fillPassword(password) {
-    cy.get('#password').type(password);
+    cy.get(this.passwordInput).type(password);
   }
 
   submit() {
-    cy.get('button[type="submit"]').click();
+    cy.get(this.buttonSubmit).click();
   }
 }
 export default new LoginPage();
