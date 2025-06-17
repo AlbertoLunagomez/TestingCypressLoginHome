e944557f3865b3037d328fd6d801c6880f40e3b0
AlbertoLunagomez <alberto.lunagomez@viaro.net>
2025-06-17 22:41:25 +0000

    feat: add calculator into home page

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -1,6 +1,13 @@
 class HomePage {
+  constructor() {
+    this.calculatorButton = '#goToCalculator';
+  }
   validateWelcomeMessage() {
     cy.contains('You are logged in!').should('be.visible');
   }
+  navigateToCalculator() {
+    cy.get(this.calculatorButton).click(); // Adjust to your navigation logic
+  }
+
 }
 export default new HomePage();
