2ddd084a11b58d724b22bc6350e4a7dc59192efa
AlbertoLunagomez <alberto.lunagomez@viaro.net>
2025-06-17 22:55:41 +0000

    refactor: create new testing for the calculator

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,16 +1,45 @@
 import calculatorPage from '../pages/calculatorPage';
 
 describe('Calculator Test', () => {
-  beforeEach(() => {
-    cy.login();
-    calculatorPage.visit(); // Adjust to your app's calculator route
-  });
+    const addSimbol = '+';
+    const subtractSimbol = '-';
+    const multiplySimbol = '*';
+    const divideSimbol = '/';
+    
+    beforeEach(() => {
+        cy.login();
+        calculatorPage.visit(); // Adjust to your app's calculator route
+    });
 
-  it('should add two numbers correctly', () => {
-    calculatorPage.enterFirstNumber(5);
-    calculatorPage.enterSecondNumber(3);
-    calculatorPage.chooseOperator('+');
-    calculatorPage.clickCalculate();
-    calculatorPage.validateResult('8');
-  });
+    it('should add two numbers correctly', () => {
+        calculatorPage.enterFirstNumber(5);
+        calculatorPage.enterSecondNumber(3);
+        calculatorPage.chooseOperator(addSimbol);
+        calculatorPage.clickCalculate();
+        calculatorPage.validateResult('8');
+    });
+
+    it('should subtract two numbers correctly', () => {
+        calculatorPage.enterFirstNumber(8);
+        calculatorPage.enterSecondNumber(6);
+        calculatorPage.chooseOperator(subtractSimbol);
+        calculatorPage.clickCalculate();
+        calculatorPage.validateResult('2');
+    });
+
+    it('should multiply numbers correctly', () => {
+        calculatorPage.enterFirstNumber(5);
+        calculatorPage.enterSecondNumber(3);
+        calculatorPage.chooseOperator(multiplySimbol);
+        calculatorPage.clickCalculate();
+        calculatorPage.validateResult('15');
+    });
+
+    it('should divide two numbers correctly', () => {
+        calculatorPage.enterFirstNumber(8);
+        calculatorPage.enterSecondNumber(2);
+        calculatorPage.chooseOperator(divideSimbol);
+        calculatorPage.clickCalculate();
+        calculatorPage.validateResult('4');
+    });
 });
